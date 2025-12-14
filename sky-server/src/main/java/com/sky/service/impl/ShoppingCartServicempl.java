8ea623d64c71f097ccad03c5b9c46d42a83a1224
kanosa0101 <1437651566@qq.com>
2025-12-14 16:34:24 +0800

    清空购物车业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServicempl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServicempl.java
@@ -93,4 +93,14 @@ public class ShoppingCartServicempl implements ShoppingCartService {
         List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
         return list;
     }
+
+    /**
+     * 清空购物车
+     */
+    @Override
+    public void cleanShoppingCart() {
+        //获取到当前微信用户的微信Id
+        Long userId = BaseContext.getCurrentId();
+        shoppingCartMapper.deleteByUserId(userId);
+    }
 }
